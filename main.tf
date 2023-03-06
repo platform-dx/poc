9aa23c367ce551f86bbe5d8447685458c91f4385
Samuel Carvalho <samuel.carvalho@thoughtworks.com>
2023-03-06 16:07:00 -0300

    luciano table

--- main.tf
+++ main.tf
@@ -49,6 +49,19 @@ resource "aws_dynamodb_table" "newdb" {
 
 }
 
+resource "aws_dynamodb_table" "lucianotable" {
+  name           = "lucianotable"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  attribute {
+    name = "noteId"
+    type = "S"
+  }
+  hash_key = "noteId"
+
+}
+
 output "my_output_arn" {
   value = aws_dynamodb_table.basic-dynamodb-table.arn
 }
