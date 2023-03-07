0b1a4bbe95dcb7401d4c1859f00e60bddf9e4be8
Samuel Carvalho <samuel.carvalho@thoughtworks.com>
2023-03-07 14:21:13 -0300

    new commit

--- main.tf
+++ main.tf
@@ -20,55 +20,8 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     type = "S"
   }
   hash_key = "noteId"
-
-}
-
-resource "aws_dynamodb_table" "othertable" {
-  name           = "newtable"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  attribute {
-    name = "noteId"
-    type = "S"
-  }
-  hash_key = "noteId"
-
-}
-
-resource "aws_dynamodb_table" "newdb" {
-  name           = "novato"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  attribute {
-    name = "noteId"
-    type = "S"
-  }
-  hash_key = "noteId"
-
-}
-
-resource "aws_dynamodb_table" "lucianotable" {
-  name           = "lucianotable"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 5
-  write_capacity = 5
-  attribute {
-    name = "noteId"
-    type = "S"
-  }
-  hash_key = "noteId"
-}
-
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "kdianadakjkafa"
-  acl    = "private"
 }
 
 output "my_output_arn" {
   value = aws_dynamodb_table.basic-dynamodb-table.arn
-}
-output "my_output_billing_mode" {
-  value = aws_dynamodb_table.basic-dynamodb-table.billing_mode
 }
