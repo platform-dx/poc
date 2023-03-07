821f66eb92b262643088476f423a857f4ffd7abf
Samuel Carvalho <samuel.carvalho@thoughtworks.com>
2023-03-07 15:17:57 -0300

    new commit again

--- main.tf
+++ main.tf
@@ -34,6 +34,18 @@ resource "aws_dynamodb_table" "basic-dynamodb-table2" {
   hash_key = "noteId"
 }
 
+resource "aws_dynamodb_table" "basic-dynamodb-table3" {
+  name           = "GameScores3"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  attribute {
+    name = "noteId"
+    type = "S"
+  }
+  hash_key = "noteId"
+}
+
 output "my_output_arn" {
   value = aws_dynamodb_table.basic-dynamodb-table.arn
 }
